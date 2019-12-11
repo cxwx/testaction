275716d93eda26ca30d6d3b217adbeed4b0485d2
chenxu <chenxu@mail.ustc.edu.cn>
2019-12-11 17:34:50 +0800

    test

---
+++ p1_test.cpp
@@ -0,0 +1,15 @@
+/**
+ * cx copyright
+ * File              : p1_test.cpp
+ * Author            : chenxu <chenxu@mail.ustc.edu.cn>
+ * Date              : 3 2019-12-11 17:32:01
+ * Last Modified Date: 3 2019-12-11 17:32:01
+ * Last Modified By  : chenxu <chenxu@mail.ustc.edu.cn>
+ */
+
+#include <iostream>
+
+int main()
+{
+	cout<<"this is a test"<<endl;
+}
